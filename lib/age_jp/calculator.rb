82097d8eefb2749c8b752774fd54782fd86450a9
ryoff <ryoffes@gmail.com>
2014-12-15 01:42:04 +0900

    add age_jp method

--- lib/age_jp/calculator.rb
+++ lib/age_jp/calculator.rb
@@ -5,23 +5,44 @@ module AgeJp
     end
 
     def age_jp
+      return unless valid_birthday?
 
+      (calculate_age(today) - calculate_age(tomorrow)).zero? ? calculate_age(today) : calculate_age(tomorrow)
     end
 
     def age
-      return unless @birthday
-      (today.strftime('%Y%m%d').to_i - @birthday.strftime('%Y%m%d').to_i) / 10000
+      return unless valid_birthday?
+
+      calculate_age(today)
     end
 
     def today
       tokyo.now.to_datetime
     end
 
+    def yesterday
+      today.yesterday
+    end
+
+    def tomorrow
+      today.tomorrow
+    end
+
     private
+
+    def calculate_age(datetime)
+      (datetime.strftime('%Y%m%d').to_i - @birthday.strftime('%Y%m%d').to_i) / 10000
+    end
 
     def tokyo
       Time.zone = 'Asia/Tokyo'
       Time.zone
+    end
+
+    def valid_birthday?
+      @birthday && @birthday.is_a?(Date)
+    rescue
+      raise ArgumentError, 'invalid birthday'
     end
   end
 end
--- spec/age_jp_spec.rb
+++ spec/age_jp_spec.rb
@@ -1,15 +1,61 @@
 require 'spec_helper'
 
 describe AgeJp do
-  let(:age_jp) { AgeJp::Calculator.new(Date.new(1999, 1, 1)) }
+  let(:age_jp) { AgeJp::Calculator.new(Date.new(2000, 1, 1)) }
 
   describe '#initialize' do
   end
 
   describe '#age_jp' do
+    context 'when today is 2014/12/30' do
+      before { Timecop.freeze(Time.new(2014, 12, 30)) }
+
+      it 'return 14' do
+        expect(age_jp.age_jp).to eq 14
+      end
+    end
+
+    context 'when today is 2014/12/31' do
+      before { Timecop.freeze(Time.new(2014, 12, 31)) }
+
+      it 'return 15' do
+        expect(age_jp.age_jp).to eq 15
+      end
+    end
+
+    context 'when today is 2015/1/1' do
+      before { Timecop.freeze(Time.new(2015, 1, 1)) }
+
+      it 'return 15' do
+        expect(age_jp.age_jp).to eq 15
+      end
+    end
   end
 
   describe '#age' do
+    context 'when today is 2014/12/30' do
+      before { Timecop.freeze(Time.new(2014, 12, 30)) }
+
+      it 'return 14' do
+        expect(age_jp.age).to eq 14
+      end
+    end
+
+    context 'when today is 2014/12/31' do
+      before { Timecop.freeze(Time.new(2014, 12, 31)) }
+
+      it 'return 14' do
+        expect(age_jp.age).to eq 14
+      end
+    end
+
+    context 'when today is 2015/1/1' do
+      before { Timecop.freeze(Time.new(2015, 1, 1)) }
+
+      it 'return 15' do
+        expect(age_jp.age).to eq 15
+      end
+    end
   end
 
   describe '#today' do
@@ -19,10 +65,37 @@ describe AgeJp do
 
     context 'when today is 2014/12/31' do
       before { Timecop.freeze(Time.new(2014, 12, 31)) }
-      it 'return 2014/12/24' do
-        expect(age_jp.today.year).to eq 2014
-        expect(age_jp.today.mon).to  eq 12
-        expect(age_jp.today.day).to  eq 31
+
+      it 'return 2014/12/31' do
+        expect(age_jp.today.strftime('%Y/%m/%d')).to eq '2014/12/31'
+      end
+    end
+  end
+
+  describe '#yesterday' do
+    it 'return instance of DateTime' do
+      expect(age_jp.yesterday).to be_a(DateTime)
+    end
+
+    context 'when today is 2014/12/31' do
+      before { Timecop.freeze(Time.new(2014, 12, 31)) }
+
+      it 'return 2014/12/30' do
+        expect(age_jp.yesterday.strftime('%Y/%m/%d')).to eq '2014/12/30'
+      end
+    end
+  end
+
+  describe '#tomorrow' do
+    it 'return instance of DateTime' do
+      expect(age_jp.tomorrow).to be_a(DateTime)
+    end
+
+    context 'when today is 2014/12/31' do
+      before { Timecop.freeze(Time.new(2014, 12, 31)) }
+
+      it 'return 2015/01/01' do
+        expect(age_jp.tomorrow.strftime('%Y/%m/%d')).to eq '2015/01/01'
       end
     end
   end
