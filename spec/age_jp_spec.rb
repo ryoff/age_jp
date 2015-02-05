57ed5196afaf6361e147cb7394ce42c7f5b557af
ryoff <ryoffes@gmail.com>
2015-02-05 11:00:42 +0900

    refactoring spec file

--- spec/age_jp_spec.rb
+++ spec/age_jp_spec.rb
@@ -8,27 +8,21 @@ describe AgeJp do
       subject(:age) { birthday.age }
 
       context 'when today is 2014/12/30' do
-        before { Timecop.freeze(Time.new(2014, 12, 30)) }
+        before { Timecop.freeze(Date.new(2014, 12, 30)) }
 
-        it 'return 14' do
-          expect(age).to eq 14
-        end
+        it { expect(age).to eq 14 }
       end
 
       context 'when today is 2014/12/31' do
-        before { Timecop.freeze(Time.new(2014, 12, 31)) }
+        before { Timecop.freeze(Date.new(2014, 12, 31)) }
 
-        it 'return 14' do
-          expect(age).to eq 14
-        end
+        it { expect(age).to eq 14 }
       end
 
       context 'when today is 2015/1/1' do
-        before { Timecop.freeze(Time.new(2015, 1, 1)) }
+        before { Timecop.freeze(Date.new(2015, 1, 1)) }
 
-        it 'return 15' do
-          expect(age).to eq 15
-        end
+        it { expect(age).to eq 15 }
       end
 
       after { Timecop.return }
@@ -40,25 +34,19 @@ describe AgeJp do
       context 'when date is 2016/12/30' do
         let(:today) { Date.new(2016, 12, 30) }
 
-        it 'return 16' do
-          expect(age_at).to eq 16
-        end
+        it { expect(age_at).to eq 16 }
       end
 
       context 'when date is 2016/12/31' do
         let(:today) { Date.new(2016, 12, 31) }
 
-        it 'return 16' do
-          expect(age_at).to eq 16
-        end
+        it { expect(age_at).to eq 16 }
       end
 
       context 'when date is 2017/1/1' do
         let(:today) { Date.new(2017, 1, 1) }
 
-        it 'return 17' do
-          expect(age_at).to eq 17
-        end
+        it { expect(age_at).to eq 17 }
       end
     end
 
@@ -66,27 +54,21 @@ describe AgeJp do
       subject(:age_jp) { birthday.age_jp }
 
       context 'when today is 2014/12/30' do
-        before { Timecop.freeze(Time.new(2014, 12, 30)) }
+        before { Timecop.freeze(Date.new(2014, 12, 30)) }
 
-        it 'return 14' do
-          expect(age_jp).to eq 14
-        end
+        it { expect(age_jp).to eq 14 }
       end
 
       context 'when today is 2014/12/31' do
-        before { Timecop.freeze(Time.new(2014, 12, 31)) }
+        before { Timecop.freeze(Date.new(2014, 12, 31)) }
 
-        it 'return 15' do
-          expect(age_jp).to eq 15
-        end
+        it { expect(age_jp).to eq 15 }
       end
 
       context 'when today is 2015/1/1' do
-        before { Timecop.freeze(Time.new(2015, 1, 1)) }
+        before { Timecop.freeze(Date.new(2015, 1, 1)) }
 
-        it 'return 15' do
-          expect(age_jp).to eq 15
-        end
+        it { expect(age_jp).to eq 15 }
       end
 
       after { Timecop.return }
@@ -98,25 +80,19 @@ describe AgeJp do
       context 'when date is 2016/12/30' do
         let(:today) { Date.new(2016, 12, 30) }
 
-        it 'return 16' do
-          expect(age_jp_at).to eq 16
-        end
+        it { expect(age_jp_at).to eq 16 }
       end
 
       context 'when date is 2016/12/31' do
         let(:today) { Date.new(2016, 12, 31) }
 
-        it 'return 17' do
-          expect(age_jp_at).to eq 17
-        end
+        it { expect(age_jp_at).to eq 17 }
       end
 
       context 'when date is 2017/1/1' do
         let(:today) { Date.new(2017, 1, 1) }
 
-        it 'return 17' do
-          expect(age_jp_at).to eq 17
-        end
+        it { expect(age_jp_at).to eq 17 }
       end
     end
   end
@@ -128,27 +104,21 @@ describe AgeJp do
       subject(:east_asian_age_reckoning) { birthday.east_asian_age_reckoning }
 
       context 'when today is 1999/12/31' do
-        before { Timecop.freeze(Time.new(1999, 12, 31)) }
+        before { Timecop.freeze(Date.new(1999, 12, 31)) }
 
-        it 'return 1' do
-          expect(east_asian_age_reckoning).to eq 1
-        end
+        it { expect(east_asian_age_reckoning).to eq 1 }
       end
 
       context 'when today is 2000/01/01' do
-        before { Timecop.freeze(Time.new(2000, 1, 1)) }
+        before { Timecop.freeze(Date.new(2000, 1, 1)) }
 
-        it 'return 2' do
-          expect(east_asian_age_reckoning).to eq 2
-        end
+        it { expect(east_asian_age_reckoning).to eq 2 }
       end
 
       context 'when today is 2000/12/31' do
-        before { Timecop.freeze(Time.new(2000, 12, 31)) }
+        before { Timecop.freeze(Date.new(2000, 12, 31)) }
 
-        it 'return 2' do
-          expect(east_asian_age_reckoning).to eq 2
-        end
+        it { expect(east_asian_age_reckoning).to eq 2 }
       end
 
       after { Timecop.return }
@@ -160,25 +130,19 @@ describe AgeJp do
       context 'when date is 2016/12/31' do
         let(:today) { Date.new(2016, 12, 31) }
 
-        it 'return 18' do
-          expect(east_asian_age_reckoning_at).to eq 18
-        end
+        it { expect(east_asian_age_reckoning_at).to eq 18 }
       end
 
       context 'when date is 2017/01/01' do
         let(:today) { Date.new(2017, 1, 1) }
 
-        it 'return 19' do
-          expect(east_asian_age_reckoning_at).to eq 19
-        end
+        it { expect(east_asian_age_reckoning_at).to eq 19 }
       end
 
       context 'when date is 2017/12/31' do
         let(:today) { Date.new(2017, 12, 31) }
 
-        it 'return 19' do
-          expect(east_asian_age_reckoning_at).to eq 19
-        end
+        it { expect(east_asian_age_reckoning_at).to eq 19 }
       end
     end
   end
@@ -186,42 +150,30 @@ describe AgeJp do
   describe "sample" do
     context "birthday is 2000/01/01. and today is 2014/12/31 " do
       let(:birthday) { Date.new(2000, 1, 1) }
-      before { Timecop.freeze(Time.new(2014, 12, 31)) }
+      before { Timecop.freeze(Date.new(2014, 12, 31)) }
 
       describe '#age' do
-        it 'return 14' do
-          expect(birthday.age).to eq 14
-        end
+        it { expect(birthday.age).to eq 14 }
       end
 
       describe '#age_at(date)' do
-        it 'return 14' do
-          expect(birthday.age_at(Date.today)).to eq 14
-        end
+        it { expect(birthday.age_at(Date.today)).to eq 14 }
       end
 
       describe '#age_jp' do
-        it 'return 15' do
-          expect(birthday.age_jp).to eq 15
-        end
+        it { expect(birthday.age_jp).to eq 15 }
       end
 
       describe '#age_jp_at(date)' do
-        it 'return 15' do
-          expect(birthday.age_jp_at(Date.today)).to eq 15
-        end
+        it { expect(birthday.age_jp_at(Date.today)).to eq 15 }
       end
 
       describe '#east_asian_age_reckoning' do
-        it 'return 15' do
-          expect(birthday.east_asian_age_reckoning).to eq 15
-        end
+        it { expect(birthday.east_asian_age_reckoning).to eq 15 }
       end
 
       describe '#east_asian_age_reckoning_at(date)' do
-        it 'return 15' do
-          expect(birthday.east_asian_age_reckoning_at(Date.today)).to eq 15
-        end
+        it { expect(birthday.east_asian_age_reckoning_at(Date.today)).to eq 15 }
       end
 
       after { Timecop.return }
