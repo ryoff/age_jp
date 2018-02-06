6a9adb4941317d4ecca3348bb0b21081ca560982
ryoff <ryoffes@gmail.com>
2018-02-06 22:29:36 +0900

    Include ActiveSupport::Testing::TimeHelpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require 'rubygems'
 require 'age_jp'
 require 'timecop'
+require "active_support/testing/time_helpers"
+
+RSpec.configure do |config|
+  config.include ActiveSupport::Testing::TimeHelpers
+end
